5a58b6f0659c36101715baf2c835ce32a033f62f
JeremyAdamHart <jeremyadamhart@gmail.com>
2018-03-06 15:13:48 -0700

    Added makefile and testing

--- Raytracer/Raytracer/main.cpp
+++ Raytracer/Raytracer/main.cpp
@@ -9,6 +9,31 @@ using namespace glm;
 
 int main() {
 
+	ShadingInfo green(0.4, 0.5, 0.4, 10, vec3(0, 1, 0), 0.5f);
+	Sphere sphereA (vec3(6.25, 4.25, -1.25), 1.f, green);
+
+	//Collision testing
+	RayIntersection a = intersect(Ray(vec3(3, 1, 2), normalize(vec3(1, 1, -1))), &sphereA );
+	if(a.intersected)
+		printf("a.t = %f\n", a.t);
+
+	RayIntersection c = intersect(Ray(vec3(1, 1, 2), normalize(vec3(1, 1, -1))), &sphereA );
+	if(c.intersected)
+		printf("c.t = %f\n", c.t);
+
+	Triangle triangleB (vec3(3, 1, -1), vec3(5, 0, 1), vec3(5, 3, 1), green);
+	RayIntersection b = intersect(Ray(vec3(1, -1, 0), normalize(vec3(1, 1, 0))), &triangleB);
+	if(b.intersected)
+		printf("b.t = %f\n", b.t);
+
+	RayIntersection d = intersect(Ray(vec3(1, -1, 2), normalize(vec3(1, 1, 0))), &triangleB);
+	if(d.intersected)
+		printf("d.t = %f\n", d.t);
+
+
+
+
+
 	Scene scene;
 	Raytracer raytracer(800, 800);
 
